0809c57bc0af88d7b09d844c767d6d60a45f1688
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-18 09:25:15 -0700

    Update find_broken_link.sh

--- find_broken_link.sh
+++ find_broken_link.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt-get install -y nodejs-legacy
 sudo apt-get install -y npm
-sudo npm install broken-link-checker -g
+npm install broken-link-checker -g
 
 # replace this with your site url.
 # r -> recursive
