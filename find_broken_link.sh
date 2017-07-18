bc6ed2b92ba7c12e2b5b93efaef1ff8fb720eec2
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-18 09:48:58 -0700

    Fix mxnet.io site url and add update

--- find_broken_link.sh
+++ find_broken_link.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+sudo apt-get update
 sudo apt-get install -y npm
 sudo apt-get install -y nodejs-legacy
 sudo npm install broken-link-checker -g
@@ -8,4 +9,4 @@ sudo npm install broken-link-checker -g
 # replace this with your site url.
 # r -> recursive
 # o -> ordered
-blc http://mxnet -ro
+blc http://mxnet.io -ro
