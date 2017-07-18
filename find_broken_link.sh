1f70c90efde45ab3e2db9eea1704a83e342f50f1
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-18 09:22:43 -0700

    Say automatic yes during install

--- find_broken_link.sh
+++ find_broken_link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-sudo apt-get install nodejs-legacy
-sudo apt-get install npm
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y npm
 sudo npm install broken-link-checker -g
 
 # replace this with your site url.
