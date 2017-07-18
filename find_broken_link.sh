7d213ae4269f22798705dacd25e64fc752eaa9d3
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-18 10:09:01 -0700

    Update find_broken_link.sh

--- find_broken_link.sh
+++ find_broken_link.sh
@@ -3,13 +3,13 @@ set -e
 
 sudo apt-get update
 
+# Install npm
+sudo apt-get install -y npm
+
 # Install nodejs- v 7.10.1
 sudo apt-get install -y curl python-software-properties
 curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
 sudo apt-get install -y nodejs
-
-# Install npm
-sudo apt-get install -y npm
 
 # Install broken link checker utility
 sudo npm install broken-link-checker -g
