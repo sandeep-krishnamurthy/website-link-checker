3aa6d4704343ff40d0ffe51a1ff0cd20a56cf530
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-18 10:04:46 -0700

    Update nodejs version from 4.x to 7.x

--- find_broken_link.sh
+++ find_broken_link.sh
@@ -2,8 +2,16 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y npm
+
+# Install nodejs- v 7.10.1
+sudo apt-get install -y curl python-software-properties
+curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
 sudo apt-get install -y nodejs
+
+# Install npm
+sudo apt-get install -y npm
+
+# Install broken link checker utility
 sudo npm install broken-link-checker -g
 
 # replace this with your site url.
