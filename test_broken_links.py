a04442667a8cbc0bd72da9b6d264a2800c44d5ad
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-08-15 13:59:31 -0700

    add more whitelisted link

--- test_broken_links.py
+++ test_broken_links.py
@@ -15,6 +15,8 @@ def prepare_link_test_result(command_output):
     BLC_UNKNOWN_REGEX = "(BLC_UNKNOWN)"
     HTTP_UNDEFINED = "HTTP_undefined"
     FALSE_SCALA_API_DOC_LINK = "java$lang.html"
+    FALSE_SCALA_API_DEPRECATED_LINK = "https://mxnet.incubator.apache.org/api/scala/docs/deprecated-list.html"
+    FALSE_PAPER_LINK = "https://static.googleusercontent.com/media/research.google.com/en/pubs/archive/45488.pdf"
 
     # Initialize flags with happy case
     current_page = ""
@@ -33,7 +35,7 @@ def prepare_link_test_result(command_output):
 
         if line.find(BROKEN_PAGE_START_REGEX) != -1:
             # Skip (401)
-            if HTTP_401_REGEX not in line and BLC_UNKNOWN_REGEX not in line and HTTP_UNDEFINED not in line and FALSE_SCALA_API_DOC_LINK not in line:
+            if HTTP_401_REGEX not in line and BLC_UNKNOWN_REGEX not in line and HTTP_UNDEFINED not in line and FALSE_SCALA_API_DOC_LINK not in line and FALSE_SCALA_API_DEPRECATED_LINK not in line and FALSE_PAPER_LINK not in line:
                 current_page_broken = True
                 current_page_broken_links += line.split(BROKEN_PAGE_START_REGEX)[1] + "\n"
 
